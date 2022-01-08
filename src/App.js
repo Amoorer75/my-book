dfdd35eb222bb59b78f192d1dcb5528ad8e7f7f5
Amoorer75 <Amoorer127@gmail.com>
2022-01-08 17:37:08 -0500

    prject init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Components/Header";
+import Book from "./Components/Book";
+import Footer from "./Components/Footer";
+import book1 from "./images/book-1.jpeg";
+import book2 from "./images/book-2.jpeg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Book
+        title="A Thousand Splendid Suns"
+        img={book1}
+        desc="Lorem ipsum dolor sit amet, duo oblique quaeque ex. Per sonet luptatum lobortis eu. Nam no dolores inimicus posidonium. Sed ne partem euismod periculis. Sed cu unum illud.
+Eum aperiri dignissim ex, in graece pericula conclusionemque quo, has ad malis nominavi. Ea modo oblique adipisci eam. Populo erroribus et mel, possit timeam dissentias vis et. Ut legendos percipitur ius. Ut ferri conceptam pri.
+"
+      />
+      <Book
+        title="The Kite Runner"
+        img={book2}
+        desc="Lorem ipsum dolor sit amet, duo oblique quaeque ex. Per sonet luptatum lobortis eu. Nam no dolores inimicus posidonium. Sed ne partem euismod periculis. Sed cu unum illud 
+Eum aperiri dignissim ex, in graece pericula conclusionemque quo, has ad malis nominavi. Ea modo oblique adipisci eam. Populo erroribus et mel, possit timeam dissentias vis et. Ut legendos percipitur ius. Ut ferri conceptam pri.
+"
+      />
+      <Footer />
     </div>
   );
 }
